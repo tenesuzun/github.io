d6a403f1d2cd28d3c837454617f00dfd48403e7f
tenesuzun <tahaenesuzun@gmail.com>
2020-10-31 14:38:48 +0300

    initial commit

---
+++ VİBF/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VİBF.Models;
+using System.Data.SqlClient;
+using Dapper;
+using System.IO;
+
+namespace VİBF.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Save(Form applicant)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=VİBF_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                connection.Open();
+                var affectedRows = connection.Execute("INSERT INTO Applicants (İsim, Soyİsim, Cinsiyet, TCNO, Telefon, Mail, Adres, DoğumTarihi, AdliSicil, MedeniDurum, Ehliyet) Values (@İsim, @Soyİsim, @Cinsiyet, @TCNO, @Telefon, @Mail, @Adres, @DoğumTarihi, @AdliSicil, @MedeniDurum, @Ehliyet);", new { İsim = applicant.Ad, Soyİsim = applicant.Soyad, Cinsiyet = applicant.Cinsiyet, TCNO = applicant.TCKN, Telefon = applicant.Telefon, Mail = applicant.Email, Adres = applicant.Adres, DoğumTarihi = applicant.Dgt, AdliSicil = applicant.Sicil, MedeniDurum = applicant.Medeni_hal, Ehliyet = applicant.Ehliyet });
+                using (MemoryStream pdf = new MemoryStream())
+                {
+                    applicant.Cv.CopyToAsync(pdf);
+                    byte[] byte_pdf = pdf.ToArray();
+                    var affectedCV = connection.Execute("INSERT INTO CVs ([File]) Values (@File);", new { File = byte_pdf });
+                }
+                
+                connection.Close();
+                return Content("Başvurunuz Tamamlandı");
+            }
+            
+        } 
+    }
+}
---
+++ VİBF/Models/Form.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace VİBF.Models
+{
+    public class Form
+    {
+        [Required(ErrorMessage ="Bu alan doldurulmak zorundadır")]
+        public string Ad { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public string Soyad { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public string Adres { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        [DataType(DataType.PhoneNumber)]
+        public long Telefon { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public long TCKN { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        [DataType(DataType.Date)]
+        public DateTime Dgt { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public string Cinsiyet { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public string Ehliyet { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public string Medeni_hal { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public string Sicil { get; set; }
+
+        [Required(ErrorMessage = "Bu alan doldurulmak zorundadır")]
+        public IFormFile Cv { get; set; }
+    }
+}
